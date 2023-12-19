4d288b8ce263f8de2c1df6a9022fe87e4d3a20bd
trixy fabrigar <t.fabrigar@gmail>
2023-12-19 14:14:33 -0800

    robot ques

---
+++ robot/robot.js
@@ -0,0 +1,65 @@
+class Robot {
+  static nameHistory = [];
+  static generateName() {
+    let name = Robot._generateLetters() + Robot._generateNumbers();
+    return name;
+  }
+
+  static _generateNumbers() {
+    let allNums = "0123456789";
+    let nums = "";
+
+    for (let count = 0; count < 3; count++) {
+      nums += allNums[Robot._randomIndex(10)];
+    }
+
+    return nums;
+  }
+
+  static _generateLetters() {
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let letters = "";
+    for (let count = 0; count < 2; count++) {
+      letters += alphabet[Robot._randomIndex(26)];
+    }
+
+    return letters;
+  }
+
+  static _randomIndex(maxLength) {
+    return Math.floor(Math.random() * maxLength);
+  }
+  constructor() {
+    this.robotName = Robot.generateName();
+    if (Robot.nameHistory.includes(this.robotName)) {
+      this.generateUniqueName();
+    }
+    Robot.nameHistory.push(this.robotName);
+  }
+
+  name() {
+    if (this.robotName) return this.robotName;
+    this.generateUniqueName();
+  }
+
+  reset() {
+    let indexToRemove = Robot.nameHistory.indexOf(this.robotName);
+    Robot.nameHistory.splice(indexToRemove, 1);
+    this.robotName = null;
+  }
+
+  generateUniqueName() {
+    let newName = Robot.generateName();
+
+    while (true) {
+      if (!Robot.nameHistory.includes(newName)) {
+        this.robotName = newName;
+        break;
+      }
+
+      newName = Robot.generateName();
+    }
+  }
+}
+
+module.exports = Robot;
