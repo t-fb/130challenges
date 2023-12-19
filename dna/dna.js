c61252c255dde511d916a20b370d523ef0237c25
trixy fabrigar <t.fabrigar@gmail>
2023-12-19 08:21:59 -0800

    dna class

---
+++ dna/dna.js
@@ -0,0 +1,27 @@
+class DNA {
+  constructor(dna) {
+    this.dna = dna;
+  }
+
+  hammingDistance(other) {
+    other = [...other];
+    let shorterStrand = [...this.dna];
+    let otherStrand = other;
+    let difference = 0;
+
+    if (other.length < this.dna) {
+      shorterStrand = other;
+      otherStrand = [...this.dna];
+    }
+
+    shorterStrand.forEach((char, i) => {
+      if (char !== otherStrand[i]) {
+        difference++;
+      }
+    });
+
+    return difference;
+  }
+}
+
+module.exports = DNA;
