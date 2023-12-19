466ca66b3f1ec9b2628cf413eaef8517d36344a4
trixy fabrigar <t.fabrigar@gmail>
2023-12-19 07:50:45 -0800

    triangle

---
+++ triangle/tri.js
@@ -0,0 +1,18 @@
+class Triangle {
+  constructor(s1, s2, s3) {
+    this.s1 = s1;
+    this.s2 = s2;
+    this.s3 = s3;
+
+    if ([s1, s2, s3].some((side) => side <= 0)) {
+      throw new Error("Invalid sides!");
+    }
+  }
+
+  kind() {
+    if (this.s1 === this.s2 && this.s1 === this.s3) {
+      return "equilateral";
+    }
+  }
+}
+module.exports = Triangle;
--- triangle/tri.test.js
+++ triangle/tri.test.js
@@ -1,5 +1,13 @@
-describe("hi", () => {
-  test("testing", () => {
-    expect(5).toBe(5);
+const Triangle = require("./tri.js");
+
+describe("triangle class", () => {
+  test("equilateral", () => {
+    let triangle = new Triangle(2, 2, 2);
+
+    expect(triangle.kind()).toBe("equilateral");
+  });
+  test("larger equilateral triangles also have equal sides", () => {
+    const triangle = new Triangle(10, 10, 10);
+    expect(triangle.kind()).toEqual("equilateral");
   });
 });
